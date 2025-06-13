cd61d5a78a2b781bca3ace11644aaf3764dfa1b5
Ethan561621 <baguiogonzaless@gmail.com>
2025-06-13 17:44:14 +0800

    Update service-worker.js
    Offline version

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,27 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker Installed');
-  e.waitUntil(
-    caches.open('static').then(cache => {
-      return cache.addAll(['./index.html', './manifest.json']);
-    })
+const CACHE_NAME = 'birthday-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://i.ibb.co/tPQsJvmn/Screenshot-2025-06-09-09-45-54-244.jpg',
+  'https://i.ibb.co/Cp0XQN3f/Screenshot-2025-06-06-09-50-15-379.jpg',
+  'https://i.ibb.co/BKPCRxpY/Screenshot-2025-05-27-09-13-48-743.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
